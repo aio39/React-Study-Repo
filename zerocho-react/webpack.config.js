9d0676c0d8b7d3e54174fa25ec4a3123d3d1723f
Aio <AioDev.JS@gmail.com>
2021-04-14 20:49:53 +0900

    webpack plugin

--- zerocho-react/webpack.config.js
+++ zerocho-react/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   name: 'WordRelay-setting',
   mode: 'development',
-  devtool: 'eval',
+  devtool: 'eval', // production에서는 hidden-source-map
   resolve: {
     extensions: ['.js', 'jsx'],
   },
@@ -16,13 +17,24 @@ module.exports = {
         test: /\.jsx?/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  browsers: ['> 5% in KR'],
+                },
+                debug: true,
+              },
+            ],
+            '@babel/preset-react',
+          ], // plugins의 모음이 presets
           plugins: ['@babel/plugin-proposal-class-properties'],
         },
       },
     ],
   },
-
+  plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'app.js',
