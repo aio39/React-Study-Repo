006dc6e03ce721b3273e367cd1d5f2f0da572071
Aio <AioDev.JS@gmail.com>
2021-04-15 12:03:22 +0900

    3-8 숫자야구 hooks로 전환하기

--- zerocho-react/ch3_NumberBaseball/NumberBaseball.jsx
+++ zerocho-react/ch3_NumberBaseball/NumberBaseball.jsx
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Try from './Try';
+
 function getNumbers() {
   const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   const array = [];
@@ -11,83 +12,70 @@ function getNumbers() {
   return array;
 }
 
-class NumberBaseball extends Component {
-  state = {
-    results: '',
-    value: '',
-    answer: getNumbers(),
-    tries: [],
-  };
+const NumberBaseball = () => {
+  const [results, setResults] = useState('');
+  const [value, setValue] = useState('');
+  const [answer, setAnswer] = useState(getNumbers());
+  const [tries, setTries] = useState([]);
 
-  onSubmitForm = (e) => {
+  const onSubmitForm = (e) => {
     e.preventDefault();
-    if (this.state.value === this.state.answer.join('')) {
-      this.setState({
-        result: '홈런',
-        tries: [
-          ...this.state.tries,
-          { try: this.state.value, result: '홈런!' },
-        ],
+    if (value === answer.join('')) {
+      setResults('홈런');
+      setTries((prevTries) => {
+        return [...prevTries, { try: value, result: '홈런!' }];
       });
     } else {
-      const answerArray = this.state.value.split('').map((v) => parseInt(v));
+      const answerArray = value.split('').map((v) => parseInt(v));
       let strike = 0;
       let ball = 0;
-      if (this.state.tries.length >= 9) {
-        this.setState({
-          result: `10번 넘게 틀려서 실패, 답은 ${this.state.answer.join(',')}`,
-        });
+      if (tries.length >= 9) {
+        setResults(`10번 넘게 틀려서 실패, 답은 ${answer.join(',')}`);
         alert('게임을 다시 시작합니다.');
-        this.setState({
-          value: '',
-          answer: getNumbers(),
-          tries: [],
-        });
+        setValue('');
+        setAnswer(getNumbers());
+        setTries([]);
       } else {
         for (let i = 0; i < 4; i++) {
-          if (answerArray[i] === this.state.answer[i]) {
+          if (answerArray[i] === answer[i]) {
             strike += 1;
-          } else if (this.state.answer.includes(answerArray[i])) {
+          } else if (answer.includes(answerArray[i])) {
             ball += 1;
           }
-          this.setState({
-            tries: [
-              ...this.state.tries,
+          setTries((prevTries) => {
+            [
+              ...tries,
               {
-                try: this.state.value,
+                try: value,
                 result: `${strike} 스트라이크, ${ball} 볼 입니다.`,
               },
-            ],
-            value: '',
+            ];
           });
         }
       }
     }
-    console.log(this.state.value);
-    console.log(this.state.answer);
+    console.log(value);
+    console.log(answer);
   };
 
-  onChangeInput = (e) => {
-    this.setState({ value: e.target.value });
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
   };
 
-  render() {
-    return (
-      <>
-        const {(results, value, tries)} = this.state;
-        <h1>{results}</h1>
-        <form onSubmit={this.onSubmitForm}>
-          <input maxLength={4} value={value} onChange={this.onChangeInput} />
-        </form>
-        <div>시도:{tries.length}</div>
-        <ul>
-          {tries.map((v, i) => {
-            return <Try key={`${i + 1}차 시도:`} tryInfo={v} index={i} />;
-          })}
-        </ul>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h1>{results}</h1>
+      <form onSubmit={onSubmitForm}>
+        <input maxLength={4} value={value} onChange={onChangeInput} />
+      </form>
+      <div>시도:{tries.length}</div>
+      <ul>
+        {tries.map((v, i) => {
+          return <Try key={`${i + 1}차 시도:`} tryInfo={v} index={i} />;
+        })}
+      </ul>
+    </>
+  );
+};
 
 export default NumberBaseball;
--- zerocho-react/ch3_NumberBaseball/Try.jsx
+++ zerocho-react/ch3_NumberBaseball/Try.jsx
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Try extends Component {
-  render() {
-    const { tryInfo } = this.props;
-    return (
-      <li>
-        <div>{this.props.tryInfo.try}</div>
-        <div>{this.props.tryInfo.result}</div>
-      </li>
-    );
-  }
-}
+const Try = (props) => {
+  const { tryInfo } = props;
+  return (
+    <li>
+      <div>{tryInfo.try}</div>
+      <div>{tryInfo.result}</div>
+    </li>
+  );
+};
 
 export default Try;
