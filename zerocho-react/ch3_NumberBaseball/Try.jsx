0caa62cd5e09c2973f822ce7c3b7687dcb0b4204
Aio <AioDev.JS@gmail.com>
2021-04-15 11:43:40 +0900

    3-7 비구조화 할당으로 this.status this.props 생략하기

--- zerocho-react/ch3_NumberBaseball/Try.jsx
+++ zerocho-react/ch3_NumberBaseball/Try.jsx
@@ -2,14 +2,11 @@ import React, { Component } from 'react';
 
 class Try extends Component {
   render() {
+    const { tryInfo } = this.props;
     return (
       <li>
-        <b>{this.props.value.fruit + this.props.value.taste}</b> -{' '}
-        {this.props.index}
-        <div>컨텐츠1</div>
-        <div>컨텐츠2</div>
-        <div>컨텐츠3</div>
-        <div>컨텐츠4</div>
+        <div>{this.props.tryInfo.try}</div>
+        <div>{this.props.tryInfo.result}</div>
       </li>
     );
   }
