868bc1bcd0af218eb16ac62e18e378cac1a3ae8a
Aio <AioDev.JS@gmail.com>
2021-04-17 18:53:31 +0900

    5-3 button에 이벤트 달아주기

--- zerocho-react/ch5_RSP/RSP.jsx
+++ zerocho-react/ch5_RSP/RSP.jsx
@@ -12,35 +12,73 @@ const scores = {
   보: 2,
 };
 
+const computerChoice = (imgCoord) => {
+  return Object.entries(rspCoords).find(function (v) {
+    return v[1] === imgCoord;
+  })[0];
+};
+
 class RSP extends Component {
   state = {
-    imgCoord: '0',
+    imgCoord: rspCoords.바위,
     result: '',
     score: 0,
   };
 
+  internal;
+
   componentDidMount() {
-    this.interval = setInterval(() => {
-      const { imgCoord } = this.state;
-      if (imgCoord === rspCoords.바위) {
-        this.setState({
-          imgCoord: rspCoords.가위,
-        });
-      } else if (imgCoord === rspCoords.가위) {
-        this.setState({
-          imgCoord: rspCoords.보,
-        });
-      } else if (imgCoord === rspCoords.보) {
-        this.setState({
-          imgCoord: rspCoords.바위,
-        });
-      }
-    }, 1000);
+    this.interval = setInterval(this.changeHand, 100);
   }
 
-  componentDidUpdate() {}
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
-  componentWillUnmount() {}
+  changeHand = () => {
+    const { imgCoord } = this.state;
+    if (imgCoord === rspCoords.바위) {
+      this.setState({
+        imgCoord: rspCoords.가위,
+      });
+    } else if (imgCoord === rspCoords.가위) {
+      this.setState({
+        imgCoord: rspCoords.보,
+      });
+    } else if (imgCoord === rspCoords.보) {
+      this.setState({
+        imgCoord: rspCoords.바위,
+      });
+    }
+  };
+
+  onClickBtn = (choice) => () => {
+    const { imgCoord } = this.state;
+    clearInterval(this.interval);
+    const myScore = scores[choice];
+    const cupScore = scores[computerChoice(imgCoord)];
+    const diff = myScore - cupScore;
+    if (diff === 0) {
+      this.setState({ result: '비겼습니다' });
+    } else if ([-1, 2].includes(diff)) {
+      this.setState((prevState) => {
+        return {
+          result: '이겼습니다.',
+          score: prevState.score + 1,
+        };
+      });
+    } else {
+      this.setState((prevState) => {
+        return {
+          result: '졌습니다.',
+          score: prevState.score - 1,
+        };
+      });
+    }
+    setTimeout(() => {
+      this.interval = setInterval(this.changeHand, 100);
+    }, 1000);
+  };
 
   render() {
     const { imgCoord, result, score } = this.state;
@@ -53,13 +91,17 @@ class RSP extends Component {
           }}
         />
         <div>
-          <button id="rock" className="btn" onClick={() => {}}>
+          <button id="rock" className="btn" onClick={this.onClickBtn('바위')}>
             바위
           </button>
-          <button id="scissor" className="btn" onClick={() => {}}>
+          <button
+            id="scissor"
+            className="btn"
+            onClick={this.onClickBtn('가위')}
+          >
             가위
           </button>
-          <button id="paper" className="btn" onClick={() => {}}>
+          <button id="paper" className="btn" onClick={this.onClickBtn('보')}>
             보
           </button>
         </div>
