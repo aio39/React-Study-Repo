45a1a5b7fa7b19a17fed7e9720a2161bb15d7d7d
Aio <AioDev.JS@gmail.com>
2021-04-17 21:01:50 +0900

    5-5 RSP Hooks

--- zerocho-react/ch5_RSP/RSP.jsx
+++ zerocho-react/ch5_RSP/RSP.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const rspCoords = {
   바위: '0',
@@ -18,98 +18,76 @@ const computerChoice = (imgCoord) => {
   })[0];
 };
 
-class RSP extends Component {
-  state = {
-    imgCoord: rspCoords.바위,
-    result: '',
-    score: 0,
-  };
+const RSP = () => {
+  const [imgCoord, setImgCoord] = useState(rspCoords.바위);
+  const [result, setResult] = useState('');
+  const [score, setScore] = useState(0);
 
-  internal;
+  const interval = useRef();
 
-  componentDidMount() {
-    this.interval = setInterval(this.changeHand, 100);
-  }
+  useEffect(() => {
+    interval.current = setInterval(changeHand, 100);
+    return () => {
+      clearInterval(interval.current);
+    };
+  }, [imgCoord]);
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  changeHand = () => {
-    const { imgCoord } = this.state;
+  const changeHand = () => {
     if (imgCoord === rspCoords.바위) {
-      this.setState({
-        imgCoord: rspCoords.가위,
-      });
+      setImgCoord(rspCoords.가위);
     } else if (imgCoord === rspCoords.가위) {
-      this.setState({
-        imgCoord: rspCoords.보,
-      });
+      setImgCoord(rspCoords.보);
     } else if (imgCoord === rspCoords.보) {
-      this.setState({
-        imgCoord: rspCoords.바위,
-      });
+      setImgCoord(rspCoords.바위);
     }
   };
 
-  onClickBtn = (choice) => () => {
-    const { imgCoord } = this.state;
-    clearInterval(this.interval);
+  const onClickBtn = (choice) => () => {
+    clearInterval(interval.current);
     const myScore = scores[choice];
     const cupScore = scores[computerChoice(imgCoord)];
     const diff = myScore - cupScore;
     if (diff === 0) {
-      this.setState({ result: '비겼습니다' });
+      setResult('비겼습니다.');
     } else if ([-1, 2].includes(diff)) {
-      this.setState((prevState) => {
-        return {
-          result: '이겼습니다.',
-          score: prevState.score + 1,
-        };
+      setResult('이겼습니다.');
+      setScore((prevState) => {
+        return prevState + 1;
       });
     } else {
-      this.setState((prevState) => {
-        return {
-          result: '졌습니다.',
-          score: prevState.score - 1,
-        };
+      setResult('졌습니다..');
+      setScore((prevState) => {
+        return prevState - 1;
       });
     }
     setTimeout(() => {
-      this.interval = setInterval(this.changeHand, 100);
+      interval.current = setInterval(changeHand, 100);
     }, 1000);
   };
 
-  render() {
-    const { imgCoord, result, score } = this.state;
-    return (
-      <>
-        <div
-          id="computer"
-          style={{
-            background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`,
-          }}
-        />
-        <div>
-          <button id="rock" className="btn" onClick={this.onClickBtn('바위')}>
-            바위
-          </button>
-          <button
-            id="scissor"
-            className="btn"
-            onClick={this.onClickBtn('가위')}
-          >
-            가위
-          </button>
-          <button id="paper" className="btn" onClick={this.onClickBtn('보')}>
-            보
-          </button>
-        </div>
-        <div>{result}</div>
-        <div>현재 {score}점</div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div
+        id="computer"
+        style={{
+          background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`,
+        }}
+      />
+      <div>
+        <button id="rock" className="btn" onClick={onClickBtn('바위')}>
+          바위
+        </button>
+        <button id="scissor" className="btn" onClick={onClickBtn('가위')}>
+          가위
+        </button>
+        <button id="paper" className="btn" onClick={onClickBtn('보')}>
+          보
+        </button>
+      </div>
+      <div>{result}</div>
+      <div>현재 {score}점</div>
+    </>
+  );
+};
 
 export default RSP;
