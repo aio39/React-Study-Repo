417e01f3cc4576a943545a12b5be958b6c9cb6b8
Aio <AioDev.JS@gmail.com>
2021-04-25 16:28:49 +0900

    #1 Item List 구현

--- nextjs-coding-devil/pages/view/[id].js
+++ nextjs-coding-devil/pages/view/[id].js
@@ -1,3 +1,42 @@
-export default function ID() {
-  return <div>ID</div>;
-}
+import { useRouter } from 'next/router';
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
+import Item from '../../src/component/Item';
+const Post = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const [item, setItem] = useState({});
+
+  const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  function getData() {
+    Axios.get(API_URL).then(res => {
+      const {
+        name,
+        image_link,
+        price,
+        description,
+        updated_at,
+        category,
+        product_type,
+        product_link,
+      } = res.data;
+      setItem({
+        name,
+        image_link,
+        price,
+        description,
+        updated_at,
+        category,
+        product_type,
+        product_link,
+      });
+    });
+  }
+  useEffect(() => {
+    if (id && id > 0) getData();
+  }, [id]);
+
+  return <Item item={item} />;
+};
+
+export default Post;
