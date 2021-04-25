385fed7962953e826152808d1cfce8dc2b5dc80e
Aio <AioDev.JS@gmail.com>
2021-04-25 19:36:07 +0900

    #1 static  기능 사용하기

--- nextjs-coding-devil/pages/index.jsx
+++ nextjs-coding-devil/pages/index.jsx
@@ -5,53 +5,39 @@ import { Divider, Header, Loader } from 'semantic-ui-react';
 import styles from '../styles/Home.module.css';
 import ItemList from '../src/component/ItemList';
 
-export default function Home() {
-  const [list, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-  function getData() {
-    Axios.get(API_URL).then(res => {
-      console.log(res.data);
-      setList(res.data);
-      setIsLoading(false);
-    });
-  }
-
-  useEffect(() => {
-    getData();
-  }, []);
-
+export default function Home({ list }) {
   return (
     <div>
       <Head>
         <title>Home</title>
         <meta name="description" content="home 화면 입니다." />
       </Head>
-      {isLoading && (
-        <div style={{ padding: '300px 0' }}>
-          <Loader inline="centered" active>
-            Loading
-          </Loader>
-        </div>
-      )}
 
-      {!isLoading && (
-        <>
-          <Header as="h3" style={{ paddingTop: 40 }}>
-            베스트 상품
-          </Header>
-          <Divider />
-          <ItemList list={list.slice(0, 9)} />
+      <>
+        <Header as="h3" style={{ paddingTop: 40 }}>
+          베스트 상품
+        </Header>
+        <Divider />
+        <ItemList list={list.slice(0, 9)} />
 
-          <Header as="h3" style={{ paddingTop: 40 }}>
-            신상품
-          </Header>
-          <Divider />
-          <ItemList list={list.slice(9)} />
-        </>
-      )}
+        <Header as="h3" style={{ paddingTop: 40 }}>
+          신상품
+        </Header>
+        <Divider />
+        <ItemList list={list.slice(9)} />
+      </>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const apiUrl = process.env.apiUrl;
+  const res = await Axios.get(apiUrl);
+  const data = res.data;
+  return {
+    props: {
+      list: data,
+      name: process.env.name,
+    },
+  };
 }
