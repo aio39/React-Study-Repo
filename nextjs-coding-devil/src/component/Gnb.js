a01665549e582ea256a306c91643865513978885
Aio <AioDev.JS@gmail.com>
2021-04-25 21:44:29 +0900

    #1 login, logout

--- nextjs-coding-devil/src/component/Gnb.js
+++ nextjs-coding-devil/src/component/Gnb.js
@@ -9,6 +9,8 @@ export default function Gnb() {
     activeItem = 'home';
   } else if (router.pathname === 'about') {
     activeItem = 'about';
+  } else if (router.pathname === 'admin') {
+    activeItem = 'admin';
   }
 
   function goLink(e, data) {
@@ -16,6 +18,8 @@ export default function Gnb() {
       router.push('/');
     } else if (data.name === 'about') {
       router.push('/about');
+    } else if (data.name === 'admin') {
+      router.push('/admin');
     }
   }
 
@@ -23,6 +27,7 @@ export default function Gnb() {
     <Menu inverted>
       <Menu.Item name="home" active={activeItem === 'home'} onClick={goLink} />
       <Menu.Item name="about" active={activeItem === 'about'} onClick={goLink} />
+      <Menu.Item name="admin" active={activeItem === 'admin'} onClick={goLink} />
     </Menu>
   );
 }
