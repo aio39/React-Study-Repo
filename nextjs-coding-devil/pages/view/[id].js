f94e445721df582d936a614cc4fa03e01f4f4eaf
Aio <AioDev.JS@gmail.com>
2021-04-25 18:09:53 +0900

    #1 loading 구현

--- nextjs-coding-devil/pages/view/[id].js
+++ nextjs-coding-devil/pages/view/[id].js
@@ -2,10 +2,12 @@ import { useRouter } from 'next/router';
 import Axios from 'axios';
 import { useEffect, useState } from 'react';
 import Item from '../../src/component/Item';
+import { Loader } from 'semantic-ui-react';
 const Post = () => {
   const router = useRouter();
   const { id } = router.query;
   const [item, setItem] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
   function getData() {
@@ -30,13 +32,26 @@ const Post = () => {
         product_type,
         product_link,
       });
+      setIsLoading(false);
     });
   }
   useEffect(() => {
     if (id && id > 0) getData();
   }, [id]);
 
-  return <Item item={item} />;
+  return (
+    <>
+      {isLoading ? (
+        <div style={{ padding: '300px 0' }}>
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>
+      ) : (
+        <Item item={item} />
+      )}
+    </>
+  );
 };
 
 export default Post;
